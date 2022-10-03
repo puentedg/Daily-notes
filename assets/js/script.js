030043a1c60e94950441d05a6ecc7a4c299ba561
puentedg <puentedg@gmail.com>
2022-10-02 22:00:41 -0400

    java added, missing display local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,3 @@
-// present the current day
-// display timeblocks for that day
-// COLOR CODED TIME BLOCKS
-// Click into a timeblock and create an event
-// save button for that time block
-// Save in local storage
-// Be able to see the events when refresh
 
 // FUNCTIONS AND EVENTS
 // function displayDate DONE
@@ -13,16 +6,101 @@
 // function retrieveEvent
 // Event Listener createEvent
 // Event Listener save btn
+
+// Variables
+let container = $(".container")
 var timeDisplayEl = $('#currentDay');
 var displayTimeblocksEl = $('#time-block');
+let timeRowEl = $('.row');
+console.log(timeRowEl);
+let userEntry =[];
+
+
 function displayDate() {
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     timeDisplayEl.text(rightNow);
   }
 
 function displayTimeblocks () {
-    displayTimeblocksEl
+    
+    for (let i=0; i<9; i++) {
+        let currentTimeEl = $(`#hour${i+1}`);
+        console.log(currentTimeEl)
+
+        let currentTime = moment().hour();
+        console.log('curr',currentTime);
+        let hour = parseInt(currentTimeEl.attr('data-time'));
+        console.log('hour', hour);
+        
+        currentTimeEl.removeClass ("past");
+        currentTimeEl.removeClass ("present");
+        currentTimeEl.removeClass ("future");
+
+        if (hour<currentTime) {
+            currentTimeEl.addClass("past");
+        }
+        else if (hour==currentTime){
+            currentTimeEl.addClass("present");
+        }
+        else {
+            currentTimeEl.addClass("future");
+        }
+    }
 }
 
+function loadNote () {
+    let hourlyNote = JSON.parse(localStorage.getItem("note"))
+    console.log(hourlyNote);
+
+    if (hourlyNote === null) {
+        return;
+    }
+    userEntry = hourlyNote;
+}
+
+function storeNote () {
+    
+    localStorage.setItem("note", JSON.stringify(userEntry));
+}
+
+function saveNote (event){
+    console.log(event) 
+
+    let button = $(event.target);
+    let buttonChildren = button.siblings();
+    let timeEl = $(this).parent()
+    let descElement = $(buttonChildren[1]);
+
+    let timeAt = timeEl.attr("data-time");
+    let description = descElement.val();
+    if (description === "") {
+        console.log("Please add note")
+        return;
+    }
+
+    console.log("saved")
+
+    let listNote = {
+        time: timeAt,
+        description: description
+    }
+    console.log(listNote);
+
+    userEntry.push(listNote);
+
+    storeNote();
+}
+
+function renderNote () {
+    var recentNotes = JSON.parse(localStorage.getItem("note"));
+    // recentNotes=userEntry.val();
+    console.log(recentNotes);
+}
+
+
+$(document).on("click", '.saveBtn', saveNote);
+
   setInterval(displayDate, 1000);
+  displayTimeblocks();
+  loadNote()
 
